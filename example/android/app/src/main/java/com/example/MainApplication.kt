7a64be2298aaa20f1b98aaf9f2d5dbdda9ee06ea
Murray Christopherson <murraychristopherson@gmail.com>
2021-07-02 12:33:59 -0700

    building on android

--- example/android/app/src/main/java/com/example/MainApplication.kt
+++ example/android/app/src/main/java/com/example/MainApplication.kt
@@ -18,7 +18,6 @@ class MainApplication : Application(), ReactApplication {
             val packages = PackageList(this).packages
             // Packages that cannot be autolinked yet can be added manually here, for example:
             // packages.add(MyReactNativePackage());
-            packages.add(RNAdyenPackage())
             return packages
         }
 
