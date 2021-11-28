1bb5ecea77e84471e2e5fbb8b5b5a41b7b0a5b1f
Murray Christopherson <murraychristopherson@gmail.com>
2021-11-27 16:09:18 -0800

    fixing template

--- example/src/environment.template.ts
+++ example/src/environment.template.ts
@@ -2,9 +2,5 @@ import { IEnvironment } from './ienvironment';
 
 export const environment: IEnvironment = {
   clientKey: '<CLIENT_KEY>',
-  merchantAccount: '<MERCHANT_ACCOUNT>',
   shopperReference: undefined,
-
-  adyenCheckoutHost: '<ADYEN_CHECKOUT_HOST>', // like 'https://checkout-test.adyen.com'
-  apiKey: '<API_KEY>',
 };
